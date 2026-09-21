bd034e5022c6025fd35d23b96241a9ce4bdd492e
AndrewRetry <andrew.retryans@gmail.com>
2026-09-21 19:26:21 +0800

    feat: initialize Student class

---
+++ backend/src/main/java/is442t1/studybuddy/student/Student.java
@@ -0,0 +1,59 @@
+package is442t1.studybuddy.student;
+
+import is442t1.studybuddy.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student extends User {
+
+    @Column(nullable = false)
+    private String name;
+
+    private String school;
+    private String program;
+    private Integer yearOfStudy;
+    private String contactNum;
+
+    public String getName() { 
+        return name; 
+    }
+
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public String getSchool() { 
+        return school; 
+    }
+
+    public void setSchool(String school) { 
+        this.school = school; 
+    }
+
+    public String getProgram() { 
+        return program; 
+    }
+
+    public void setProgram(String program) { 
+        this.program = program; 
+    }
+
+    public Integer getYearOfStudy() { 
+        return yearOfStudy; 
+    }
+
+    public void setYearOfStudy(Integer yearOfStudy) { 
+        this.yearOfStudy = yearOfStudy; 
+    }
+
+    public String getContactNum() { 
+        return contactNum; 
+    }
+
+    public void setContactNum(String contactNum) { 
+        this.contactNum = contactNum; 
+    }
+}
---
+++ backend/src/main/java/is442t1/studybuddy/student/StudentRepository.java
@@ -0,0 +1,13 @@
+package is442t1.studybuddy.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+    Optional<Student> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
